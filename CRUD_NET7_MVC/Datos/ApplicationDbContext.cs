8fe5c0a4eb32312f8328d3278bd54d57f76de0c1
i202015638@cibertec.edu.pe <i202015638@cibertec.edu.pe>
2024-04-11 21:41:54 -0500

    CRUD Contactos

---
+++ CRUD_NET7_MVC/Datos/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CRUD_NET7_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_NET7_MVC.Datos
+{
+    public class ApplicationDbContext: DbContext
+    {
+        //Creando archivo de contexto
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Espacio para los modelos 
+        public DbSet<Contacto> Contacto {  get; set; }
+
+    }
+}
---
+++ CRUD_NET7_MVC/Program.cs
@@ -0,0 +1,35 @@
+using CRUD_NET7_MVC.Datos;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Configuración de conexión a SQL ser local db MSSSQLLOCAL
+builder.Services.AddDbContext<ApplicationDbContext>(opciones =>
+    opciones.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSql"))
+);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Inicio}/{action=Index}/{id?}");
+
+app.Run();
